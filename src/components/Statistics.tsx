118efbb9d98b35671455f8e9fbce4e74b8ad9fe9
Whitespace <grs2143@mail.ru>
2023-02-10 19:29:27 +1000

    remove y-scroll

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -7,29 +7,27 @@ import UISendApplication from '../UIKIT/UISendApplication';
 const Statistics = (props: any) => {
   const [send, setSend] = useState<boolean>(false);
   return (
-    <div className={classes['statistics']} ref={props.staticRef}>
-      <Layout>
+      <div className={classes['statistics']} ref={props.staticRef}>
         <UISendApplication send={send} setSend={setSend} title={'Зарегистрироваться'} />
         <div className={classes['statistics__item']}>
-        <div className={classes['statistics__left']}>
-          <img className={classes['statistics__left__img']} src="logoAutoBig.png" alt="logoAuto" />
-          <div className={classes['statistics__left__title']}>Статистика и аукционы</div>
-          <div className={classes['statistics__left__text']}>
-            Бесплатно зарегистрируйтесь в нашем сервисе и получите бесплатный доступ на
-            автостатистику и аукционы Японии.
+          <div className={classes['statistics__left']}>
+            <img className={classes['statistics__left__img']} src="logoAutoBig.png" alt="logoAuto" />
+            <div className={classes['statistics__left__title']}>Статистика и аукционы</div>
+            <div className={classes['statistics__left__text']}>
+              Бесплатно зарегистрируйтесь в нашем сервисе и получите бесплатный доступ на
+              автостатистику и аукционы Японии.
+            </div>
+            <div className={classes['statistics__buttonBlock']}>
+              <div className={classes['statistics__left__button']} onClick={() => setSend(true)}>
+                <UIButton buttonText={'Зарегистрироваться'} />
+              </div>
+            </div>
           </div>
-          <div className={classes['statistics__buttonBlock']}>
-          <div className={classes['statistics__left__button']} onClick={() => setSend(true)}>
-            <UIButton buttonText={'Зарегистрироваться'} />
-          </div>
+          <div className={classes['statistics__right']}>
+            <img width="650" height="650" src="gtr.png" alt="gtr" />
           </div>
         </div>
-        <div className={classes['statistics__right']}>
-          <img width="650" height="650" src="gtr.png" alt="gtr" />
-        </div>
-        </div>
-      </Layout>
-    </div>
+      </div>
   );
 };
 
