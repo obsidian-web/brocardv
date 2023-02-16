618148b5dd6c70ba66d0ab5bf9ca34bad3e30517
Whitespace <grs2143@mail.ru>
2023-02-16 20:14:24 +1000

    updated routers

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
 const router = require('express').Router()
 
-const categoryRouter = require('./ModelRouter')
+const modelRouter = require('./ModelRouter')
 const vehicleRouter = require('./VehicleRouter')
 const userRouter = require('./UserRouter')
 const reviewRouter = require('./ReviewRouter')
+const manufacturerRouter = require('./ManufacturerRouter')
+const sideRouter = require('./SideRouter')
 
 //connect routes with routers
-router.use('/category', categoryRouter)
-router.use('/user', userRouter)
+router.use('/model', modelRouter)
+router.use('/manufacturer', manufacturerRouter)
+router.use('/side', sideRouter)
 router.use('/vehicle', vehicleRouter)
+router.use('/user', userRouter)
 router.use('/review', reviewRouter)
 
 module.exports = router
